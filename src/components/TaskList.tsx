80152ad30d8f1b1c36f5ebb61238b291337813d4
Brunoskyy <arturbrunoferreira@gmail.com>
2022-01-13 15:05:25 -0300

    Apply Tasklist hooks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,39 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) {
+      return;
+    }
+    
+    const newTask: Task = {
+      id: Date.now(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if(task.id !== id) {
+        return task;
+      }
+
+      const checkStatus = task.isComplete;
+
+      task.isComplete = !checkStatus;
+      return task;
+    });
+
+    setTasks(newTasks);
   }
 
+
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
